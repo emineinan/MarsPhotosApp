ca95d13b94f4a815bac3b038f5b6b004ce3981c9
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-05-14 12:33:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/marsphotosapp/overview/OverviewViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.marsphotosapp.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+    private val _status = MutableLiveData<String>()
+    val status: LiveData<String> = _status
+
+    init {
+        getMarsPhotos()
+    }
+
+    private fun getMarsPhotos() {
+        _status.value = "Set the Mars API status response here!"
+    }
+}
